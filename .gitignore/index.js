8ef1c951a73c0c423482be9a044e9719f3630747
LeGamerDu06238 <35636236+LeGamerDu06238@users.noreply.github.com>
2018-07-16 14:56:27 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -5,6 +5,10 @@ var bot = new Discord.Client();
 bot.on("ready", function() {
     bot.user.setGame("LesLoup, <help");
     console.log("Le bot a bien ete connecte");
+  
+bot.on("message"), function (message) {
+    if_(message.content === "<ping") {
+        message.reply("pong")
 });
 
 bot.login("NDY4MDcxNjg4NjY4NDQ2NzQw.Diz1Qg.82Sw_tI05Y9ZeZmiLd-I29SamfQ");
